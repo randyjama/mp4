aba57349e007572d033277ff0d068f6772fc123c
Randy Jama <randyjama@gmail.com>
2017-11-12 19:13:37 -0800

    1.3 Variables portion finished

--- src/test/java/ca/ubc/ece/cpen221/mp4/tests/SimpleCalculatorTest.java
+++ src/test/java/ca/ubc/ece/cpen221/mp4/tests/SimpleCalculatorTest.java
@@ -13,11 +13,13 @@ import org.junit.Test;
 
 import ca.ubc.ece.cpen221.mp4.expression.AbsoluteValue;
 import ca.ubc.ece.cpen221.mp4.expression.Addition;
+import ca.ubc.ece.cpen221.mp4.expression.BinaryExpression;
 import ca.ubc.ece.cpen221.mp4.expression.Division;
 import ca.ubc.ece.cpen221.mp4.expression.Exponentiation;
 import ca.ubc.ece.cpen221.mp4.expression.Expression;
 import ca.ubc.ece.cpen221.mp4.expression.Multiplication;
 import ca.ubc.ece.cpen221.mp4.expression.Negation;
+import ca.ubc.ece.cpen221.mp4.expression.NumberExpression;
 import ca.ubc.ece.cpen221.mp4.expression.Subtraction;
 import ca.ubc.ece.cpen221.mp4.expression.VariableExpression;
 import ca.ubc.ece.cpen221.mp4.operator.Operator;
@@ -32,7 +34,6 @@ public class SimpleCalculatorTest {
 
 	// TODO Add more tests
 
-	@SuppressWarnings("deprecation")
 	@Test
 	public void test0() {
 		
@@ -47,15 +48,22 @@ public class SimpleCalculatorTest {
 		operatorSet.add(new Negation());
 		operatorSet.add(new Exponentiation());
 		
-
+/*
 		ExpressionParser parser = new ExpressionParser(operatorSet, new ExpressionMaker());
 		
-		String expression = "3 * 3 - 2";
+		String expression = "x * x - 2";
 		VariableExpression var = new VariableExpression("x");
 		var.store(3);
 		Expression exp = parser.parse(expression);
+*/
 		
-		assertEquals(7, var.eval(), 0.5);
+		VariableExpression var = new VariableExpression("x");
+		var.store(3);
+		Expression exp = new BinaryExpression (new Subtraction(), 
+				new BinaryExpression(new Multiplication(), var, var), 
+				new NumberExpression(2));
+		
+		assertEquals(7, exp.eval(), 0.01);
 	}
 
 }
