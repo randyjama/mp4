1af00dfb772b7dea79bb9960a8e8b88448f9044d
Randy Jama <randyjama@gmail.com>
2017-11-13 21:13:31 -0800

    Newton's method not working

--- src/main/java/ca/ubc/ece/cpen221/mp4/expression/DerivativeExpression.java
+++ src/main/java/ca/ubc/ece/cpen221/mp4/expression/DerivativeExpression.java
@@ -5,51 +5,93 @@ package ca.ubc.ece.cpen221.mp4.expression;
  *
  */
 public class DerivativeExpression implements Expression {
-	
+
 	private Expression fn;
 	private VariableExpression independentVar;
-	private Expression derv;
 	private double DELTA_X = 1e-9;
-	
-		
+
 	/**
-	 * Create an expression representing the derivative of the specified
-	 * function with respect to the specified variable.
+	 * Create an expression representing the derivative of the specified function
+	 * with respect to the specified variable.
 	 * 
-	 * @param fn the function whose derivative this expression represents
-	 * @param independentVar the variable with respect to which we're
-	 * 		  differentiating
+	 * @param fn
+	 *            the function whose derivative this expression represents
+	 * @param independentVar
+	 *            the variable with respect to which we're differentiating
 	 */
-	public DerivativeExpression(Expression fn, 
-					VariableExpression independentVar) {
+	public DerivativeExpression(Expression fn, VariableExpression independentVar) {
 		// TODO implement this constructor
 		this.fn = fn;
 		this.independentVar = independentVar;
-		/*derv = new BinaryExpression (new Division(),
-				new BinaryExpression (new Subtraction(), fnDelta, fn),
-				new NumberExpression(DELTA_X));
-		*/
+		/*
+		 * derv = new BinaryExpression (new Division(), new BinaryExpression (new
+		 * Subtraction(), fnDelta, fn), new NumberExpression(DELTA_X));
+		 */
 	}
-	
+
 	@Override
 	public double eval() {
 		// TODO implement this method
 		// f(x)
 		double fx = fn.eval();
-		
+
 		// f(x+dx)
 		independentVar.store(independentVar.eval() + DELTA_X);
 		double fxdx = fn.eval();
-		
-		return (fxdx - fx)/DELTA_X; // derivative formula
+
+		return (fxdx - fx) / DELTA_X; // derivative formula
 	}
-	
+
+	/*
+	 * @return result shows the format: d/d<independent variable> <function>
+	 */
 	@Override
 	public String toString() {
 		// TODO implement this method
-		
-		return null;
+		String result = "d/d" + independentVar + " " + fn.toString();
+		return result;
 	}
-	
 
+	/**
+	 * Returns a zero of the specified function using Newton's method with
+	 * approxZero as the initial estimate.
+	 * 
+	 * @param fn
+	 *            the function whose zero is to be found; must be convergent
+	 * @param x
+	 *            the independent variable of the function
+	 * @param approxZero
+	 *            initial approximation for the zero of the function
+	 * @param tolerance
+	 *            how close to zero the returned zero must be
+	 * @return result a double containing one root of the function +/- given
+	 *         tolerance
+	 */
+	public double newtonsMethod(Expression fn, VariableExpression x, 
+			double approxZero, double tolerance) {
+		DerivativeExpression derv = new DerivativeExpression(fn, x);
+		x.store(approxZero); // x0
+		System.out.println(fn.eval()); // debugging
+		System.out.println(derv.eval()); // debugging
+		
+		// xn+1 = xn - f(xn)/f'(xn)
+		while (Math.abs(fn.eval()) > tolerance) {
+			approxZero = approxZero - fn.eval()/derv.eval();
+			x.store(approxZero);
+		}
+
+		return approxZero;
+	}
 }
+
+/*		
+//base case
+if (Math.abs(fn.eval()) <= tolerance) {
+	return approxZero;
+}
+else {
+	return newtonMethod (fn, x, approxZero, tolerance);
+			approxZero - fn.eval()/derv.eval();
+}
+}
+*/
--- src/test/java/ca/ubc/ece/cpen221/mp4/tests/SimpleCalculatorTest.java
+++ src/test/java/ca/ubc/ece/cpen221/mp4/tests/SimpleCalculatorTest.java
@@ -31,62 +31,79 @@ import ca.ubc.ece.cpen221.mp4.parser.ExpressionParser;
 
 // TODO you will need to import other packages that has code to test
 
-
 public class SimpleCalculatorTest {
 
 	// TODO Add more tests
 
+	// test function and derivative evaluations for a given independent variable
+	// value
 	@Test
 	public void test0() {
-		
-		Set<Operator> operatorSet = new HashSet<Operator>();
 
-		// TODO add operators to operatorSet
-		operatorSet.add(new Addition());
-		operatorSet.add(new Subtraction());
-		operatorSet.add(new Division());
-		operatorSet.add(new Multiplication());
-		operatorSet.add(new AbsoluteValue());
-		operatorSet.add(new Negation());
-		operatorSet.add(new Exponentiation());
-		
-/*
-		ExpressionParser parser = new ExpressionParser(operatorSet, new ExpressionMaker());
-		
-		String expression = "x * x - 2";
-		VariableExpression var = new VariableExpression("x");
-		var.store(3);
-		Expression exp = parser.parse(expression);
-*/
-		
+		Set<Operator> operatorSet = new HashSet<Operator>();
+		/*
+		 * // TODO add operators to operatorSet operatorSet.add(new Addition());
+		 * operatorSet.add(new Subtraction()); operatorSet.add(new Division());
+		 * operatorSet.add(new Multiplication()); operatorSet.add(new AbsoluteValue());
+		 * operatorSet.add(new Negation()); operatorSet.add(new Exponentiation());
+		 */
+		/*
+		 * ExpressionParser parser = new ExpressionParser(operatorSet, new
+		 * ExpressionMaker());
+		 * 
+		 * String expression = "x * x - 2"; VariableExpression var = new
+		 * VariableExpression("x"); var.store(3); Expression exp =
+		 * parser.parse(expression);
+		 */
+
 		// exp1 = x * x - 2
 		VariableExpression var = new VariableExpression("x");
 		var.store(3);
-		Expression exp = new BinaryExpression (new Subtraction(), 
-				new BinaryExpression(new Multiplication(), var, var), 
+		Expression exp = new BinaryExpression(new Subtraction(), new BinaryExpression(new Multiplication(), var, var),
 				new NumberExpression(2));
-		
+
 		// derv for exp1 = 2x
 		DerivativeExpression derv = new DerivativeExpression(exp, var);
 		System.out.println(derv.eval());
-		
+
 		assertEquals(7, exp.eval(), 0.01);
 		assertEquals(6, derv.eval(), 0.01);
-		
+
 		// exp2 = x^3 + 5x^2
 		var = new VariableExpression("x");
 		var.store(2);
-		exp = new BinaryExpression(new Addition(), 
-				new BinaryExpression (new Exponentiation(), var, new NumberExpression(3)), 
-				new BinaryExpression (new Multiplication(), new NumberExpression(5),
-						new BinaryExpression(new Exponentiation(),var, new NumberExpression(2))));
-		
-		//derv for exp2 = 3x^2 + 10x
+		exp = new BinaryExpression(new Addition(),
+				new BinaryExpression(new Exponentiation(), var, new NumberExpression(3)),
+				new BinaryExpression(new Multiplication(), new NumberExpression(5),
+						new BinaryExpression(new Exponentiation(), var, new NumberExpression(2))));
+
+		// derv for exp2 = 3x^2 + 10x
 		DerivativeExpression derv2 = new DerivativeExpression(exp, var);
 		System.out.println(derv2.eval());
-		
+
 		assertEquals(28, exp.eval(), 0.01);
 		assertEquals(32, derv2.eval(), 0.01);
+
+		System.out.println(derv2.toString());
+	}
+
+	// test roots of function
+	@Test
+	public void test2() {
+
+		// exp1 = x^2 - 3x + 2
+		VariableExpression var = new VariableExpression("x");
+		Expression exp = new BinaryExpression(new Subtraction(),
+				new BinaryExpression(new Exponentiation(), var, new NumberExpression(2)),
+				new BinaryExpression(new Addition(),
+						new BinaryExpression(new Multiplication(), var, new NumberExpression(3)),
+						new NumberExpression(2)));
+
+		// derv for exp1 = 2x - 3
+		DerivativeExpression derv = new DerivativeExpression(exp, var);
+
+		// see if newtons method returns 1 or 2 for above fn
+		assertEquals(2, derv.newtonsMethod(exp, var, 2.5, 0.0001), 0.001);
 	}
 
 }
